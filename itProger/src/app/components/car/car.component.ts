73b4cd079669994098ca4ea36ddff66014503d6f
Asya19 <asya.kisko@yandex.ru>
2024-02-11 14:24:52 +0300

    feat: add properties, interface and method

--- itProger/src/app/components/car/car.component.ts
+++ itProger/src/app/components/car/car.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-car',
   templateUrl: './car.component.html',
   styleUrl: './car.component.css'
 })
-export class CarComponent {
 
+
+// export - значит, все что будет в классе можно будет экспортировать, например в HTML
+// Данный класс должен реализовывать метод OnInit
+export class CarComponent implements OnInit {
+
+  name:string;
+  speed:number;
+  model:string;
+  colors:Colors;
+
+  options:string[];
+
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.name = 'BMW';
+    this.speed = 235;
+    this.model = 'RS8';
+    this.colors = {
+      car: 'Белый',
+      salon: 'Чёрный',
+      wheels: 'Зелёные'
+    };
+    this.options = ['ABS', 'Автопилот', 'Авто Паркинг']
+  }
+
+
+}
+interface Colors {
+  car:string;
+  salon:string;
+  wheels:string;
 }
